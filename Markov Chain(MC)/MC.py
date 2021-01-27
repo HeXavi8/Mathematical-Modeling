1300285e2db9502bebeec452d956f05af16be376
Xavi He <51332043+HeXavi8@users.noreply.github.com>
2021-01-27 10:31:10 +0800

    Update MC.py

--- Markov Chain(MC)/MC.py
+++ Markov Chain(MC)/MC.py
@@ -1,4 +1,4 @@
-#The situation and possibility of translation are showed in MC.img
+#The situation and possibility of translation are showed in MC.jpg
 
 n = 5  # Calculate the situation after 5 minutes or hours or days or months or years
 v = (1, 0, 0, 0)  # the situation at 0 minutes or hours or days or months or years
